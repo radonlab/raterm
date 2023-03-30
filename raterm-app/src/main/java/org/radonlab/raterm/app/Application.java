ac047b28c2e4b0f20662a4d0d6fb52cd61b9e1ef
skylerlee <skyler.ac.lee@gmail.com>
2023-03-31 01:34:59 +0800

    Init update

--- raterm-app/src/main/java/org/radonlab/raterm/app/Application.java
+++ raterm-app/src/main/java/org/radonlab/raterm/app/Application.java
@@ -1,7 +1,18 @@
 package org.radonlab.raterm.app;
 
+import javax.swing.*;
+
 public class Application {
+    private static JPanel createMainPanel() {
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello App!");
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Basic Terminal Shell Example");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setContentPane(createMainPanel());
+            frame.pack();
+            frame.setVisible(true);
+        });
     }
 }
