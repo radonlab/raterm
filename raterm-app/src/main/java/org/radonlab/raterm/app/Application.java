4a2b873032ca6a2800f15b76190fcb4c2a8b0a5c
skylerlee <skyler.ac.lee@gmail.com>
2023-04-05 03:32:39 +0800

    feat: add ui

--- raterm-app/src/main/java/org/radonlab/raterm/app/Application.java
+++ raterm-app/src/main/java/org/radonlab/raterm/app/Application.java
@@ -1,9 +1,15 @@
 package org.radonlab.raterm.app;
 
+import org.radonlab.raterm.terminal.ui.JediTermWidget;
+import org.radonlab.raterm.terminal.ui.settings.DefaultSettingsProvider;
+
 import javax.swing.*;
 
 public class Application {
     private static JPanel createMainPanel() {
+        JediTermWidget widget = new JediTermWidget(80, 24, new DefaultSettingsProvider());
+//        widget.start();
+        return widget;
     }
 
     public static void main(String[] args) {
