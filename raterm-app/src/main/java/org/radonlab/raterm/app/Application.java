02250d461bdbbbbb75aa82ce4fbd691d004a1b71
skylerlee <skyler.ac.lee@gmail.com>
2023-04-06 00:16:50 +0800

    fix: add missing code

--- raterm-app/src/main/java/org/radonlab/raterm/app/Application.java
+++ raterm-app/src/main/java/org/radonlab/raterm/app/Application.java
@@ -1,15 +1,43 @@
 package org.radonlab.raterm.app;
 
+import com.google.common.collect.Maps;
+import com.pty4j.PtyProcess;
+import com.pty4j.PtyProcessBuilder;
+import org.radonlab.raterm.pty.PtyProcessTtyConnector;
+import org.radonlab.raterm.terminal.TtyConnector;
 import org.radonlab.raterm.terminal.ui.JediTermWidget;
+import org.radonlab.raterm.terminal.ui.UIUtil;
 import org.radonlab.raterm.terminal.ui.settings.DefaultSettingsProvider;
 
 import javax.swing.*;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
 public class Application {
+    private static TtyConnector createTty() throws IOException {
+        Map<String, String> envVars = Maps.newHashMap(System.getenv());
+        String[] command;
+        if (UIUtil.isWindows) {
+            command = new String[]{"cmd.exe"};
+        } else {
+            command = new String[]{"/bin/bash", "--login"};
+            envVars.put("TERM", "xterm-256color");
+        }
+        PtyProcess process = new PtyProcessBuilder().setCommand(command).setEnvironment(envVars).start();
+        return new PtyProcessTtyConnector(process, StandardCharsets.UTF_8);
+    }
+
     private static JPanel createMainPanel() {
-        JediTermWidget widget = new JediTermWidget(80, 24, new DefaultSettingsProvider());
-//        widget.start();
-        return widget;
+        try {
+            JediTermWidget widget = new JediTermWidget(80, 24, new DefaultSettingsProvider());
+            widget.setTtyConnector(createTty());
+            widget.start();
+            return widget;
+        } catch (IOException e) {
+            System.out.println(e);
+            return null;
+        }
     }
 
     public static void main(String[] args) {
