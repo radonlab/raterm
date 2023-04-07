92ade5152d1c34b5a608b885b891c35f79547991
skylerlee <skyler.ac.lee@gmail.com>
2023-04-08 05:16:08 +0800

    build: fix logging

--- raterm-app/src/main/java/org/radonlab/raterm/pref/Preference.java
+++ raterm-app/src/main/java/org/radonlab/raterm/pref/Preference.java
@@ -3,12 +3,15 @@ package org.radonlab.raterm.pref;
 import com.fasterxml.jackson.dataformat.toml.TomlMapper;
 import dev.dirs.ProjectDirectories;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @Data
+@Slf4j
 public class Preference {
     public static final Preference defaultPreference = new Preference("Dark", "Mono");
 
@@ -20,13 +23,14 @@ public class Preference {
         this.font = font;
     }
 
-    public static Preference loadPreference() {
+    public static @NotNull Preference loadPreference() {
         try {
             String configDir = ProjectDirectories.from("org", "radonlab", "raterm").configDir;
             Path confFile = Paths.get(configDir, "config.toml");
             TomlMapper mapper = new TomlMapper();
             return mapper.readValue(confFile.toFile(), Preference.class);
         } catch (IOException e) {
+            log.warn("Cannot load preference file", e);
             return defaultPreference;
         }
     }
