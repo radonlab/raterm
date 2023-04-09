2b6129f3bd2749ed826c48739f58f32ebc920919
skylerlee <skyler.ac.lee@gmail.com>
2023-04-10 00:46:53 +0800

    feat: update

--- raterm-app/src/main/java/org/radonlab/raterm/tab/ui/GoldenTabPane.java
+++ raterm-app/src/main/java/org/radonlab/raterm/tab/ui/GoldenTabPane.java
@@ -4,6 +4,8 @@ import lombok.Setter;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import javax.swing.plaf.basic.BasicTabbedPaneUI;
+import java.awt.*;
 
 public class GoldenTabPane extends JTabbedPane {
 
@@ -12,6 +14,18 @@ public class GoldenTabPane extends JTabbedPane {
 
     public GoldenTabPane(@NotNull TabCallback callback) {
         this.callback = callback;
+        this.setUI(new TabPaneUI());
         this.addTab("Tab1", this.callback.createTab());
+    }
+
+    private static class TabPaneUI extends BasicTabbedPaneUI {
+        @Override
+        protected void paintTab(Graphics g, int tabPlacement, Rectangle[] rects,
+                                int tabIndex, Rectangle iconRect, Rectangle textRect) {
+            super.paintTab(g, tabPlacement, rects, tabIndex, iconRect, textRect);
+            Rectangle tabRect = rects[tabIndex];
+            g.setColor(Color.MAGENTA);
+            g.drawRect(tabRect.x, tabRect.y, tabRect.width, tabRect.height);
+        }
     }
 }
