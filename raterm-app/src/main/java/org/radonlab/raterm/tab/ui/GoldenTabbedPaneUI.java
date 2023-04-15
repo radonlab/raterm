c5af7aece6edbfd596946f11788b46ca4440c556
skylerlee <skyler.ac.lee@gmail.com>
2023-04-16 04:32:31 +0800

    fix: fix rendering

--- raterm-app/src/main/java/org/radonlab/raterm/tab/ui/GoldenTabbedPaneUI.java
+++ raterm-app/src/main/java/org/radonlab/raterm/tab/ui/GoldenTabbedPaneUI.java
@@ -1,16 +1,23 @@
 package org.radonlab.raterm.tab.ui;
 
+import com.google.common.collect.ImmutableMap;
+
 import javax.swing.plaf.basic.BasicTabbedPaneUI;
 import java.awt.*;
 import java.awt.geom.Arc2D;
 import java.awt.geom.GeneralPath;
 import java.awt.geom.Point2D;
+import java.util.Map;
 
 public class GoldenTabbedPaneUI extends BasicTabbedPaneUI {
     private static final int tabWidth = 240;
     private static final float tabRadius = 8f;
     private static final Color tabSelectedColor = new Color(0xffffff);
     private static final Color tabHoverColor = new Color(0xe0e0e0);
+    private static final Map<RenderingHints.Key, Object> renderingHints = ImmutableMap.of(
+            RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON,
+            RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_NORMALIZE
+    );
 
     @Override
     protected int calculateTabWidth(int tabPlacement, int tabIndex, FontMetrics metrics) {
@@ -25,17 +32,34 @@ public class GoldenTabbedPaneUI extends BasicTabbedPaneUI {
             return;
         }
         // Trigger repainting explicitly
-        tabPane.repaint();
+        repaintTab(oldIndex);
+        repaintTab(index);
     }
 
     @Override
     protected void paintTabBackground(Graphics g, int tabPlacement, int tabIndex, int x, int y, int w, int h, boolean isSelected) {
         Graphics2D g2 = (Graphics2D) g;
+        // Enable anti-aliasing
+        g2.setRenderingHints(renderingHints);
         // paint tab background
         Color background = getTabBackground(tabIndex, isSelected);
         Shape path = getTabPath(x, y, w, h);
         g2.setColor(background);
         g2.fill(path);
+    }
+
+    private void repaintTab(int tabIndex) {
+        if (tabIndex < 0 || tabIndex >= tabPane.getTabCount()) {
+            return;
+        }
+        Rectangle bounds = getTabBounds(tabPane, tabIndex);
+        if (bounds == null) {
+            return;
+        }
+        // Increase the repaint region to include some extra parts
+        bounds.x -= tabRadius;
+        bounds.width += tabRadius * 2;
+        tabPane.repaint(bounds);
     }
 
     private Color getTabBackground(int tabIndex, boolean isSelected) {
